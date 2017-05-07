fe4112a3f684b657067b479f6c40a47e047dc226
Scott Russell <scott.tkdmaster@mac.com>
2017-05-06 23:23:52 -0400

    New File
    Beginning work on creating separate class for capturing initial hand
    image (the key) and saving that image into the working directory. From
    there, the main class should run the loop until the hand in the real
    time image matches that of the image saved in the working directory.

--- hand_key_capture.m
+++ hand_key_capture.m
@@ -3,7 +3,7 @@ clear all;
 close all;
 clc;
 fontSize = 14;
-count = 0;
+run = true;
 
 %Thresholds for blue glove color
 hueThresholdLow = 0.4;
@@ -19,53 +19,43 @@ vid = webcam(1);
 pause(2);
 preview(vid);
 
-while (count ~= 200)
-    display(count);
-    
-    img = snapshot(vid);
-    %Wait for user to capture image
-    % choice = questdlg('Press "capture" when you are ready to take a photo.', 'Take Picture', 'Capture', 'Cancel', 'Capture');
-    % switch choice
-    %     case 'Capture'
-    %         img = snapshot(vid);
-    %     case 'Cancel'
-    %         return;
-    % end
-
-    %Break down original image into HSV values
-    %and seperate hand usingthresholds
-    hsvImage = rgb2hsv(img);
-    hImage = hsvImage(:,:,1);
-    sImage = hsvImage(:,:,2);
-    vImage = hsvImage(:,:,3);
-    hueMask = (hImage >= hueThresholdLow) & (hImage <= hueThresholdHigh);
-    saturationMask = (sImage >= saturationThresholdLow) & (sImage <= saturationThresholdHigh);
-    valueMask = (vImage >= valueThresholdLow) & (vImage <= valueThresholdHigh);
-
-    %Putting it altogether into one image
-    coloredObjectsMask = uint8(hueMask & saturationMask & valueMask);
-
-    %Getting rid of excess pixels, smoothing borders, and filling regions
-    smallestAcceptableArea = 100;
-    coloredObjectsMask = uint8(bwareaopen(coloredObjectsMask, smallestAcceptableArea));
-    structuringElement = strel('disk', 4);
-    coloredObjectsMask = imclose(coloredObjectsMask, structuringElement);
-    coloredObjectsMask = imfill(logical(coloredObjectsMask), 'holes');
-
-    %Getting rectangle around hand region
-    imshow(img);
-    hold on;
-    measurements = regionprops(coloredObjectsMask);
-    numberOfRegions = size(measurements, 1);
-    for k = 1 : numberOfRegions
-        thisBox = measurements(k).BoundingBox;
-        rectangle('position', thisBox(:), 'Edgecolor', 'r');
-    end
-    hold off;
-    
-    count = count + 1;
+choice = questdlg('Press "capture" when you are ready to take a photo.', 'Take Picture', 'Capture', 'Cancel', 'Capture');
+switch choice
+    case 'Capture'
+        img = snapshot(vid);
+    case 'Cancel'
+        return;
 end
 
+%Break down original image into HSV values
+%and seperate hand usingthresholds
+hsvImage = rgb2hsv(img);
+hImage = hsvImage(:,:,1);
+sImage = hsvImage(:,:,2);
+vImage = hsvImage(:,:,3);
+hueMask = (hImage >= hueThresholdLow) & (hImage <= hueThresholdHigh);
+saturationMask = (sImage >= saturationThresholdLow) & (sImage <= saturationThresholdHigh);
+valueMask = (vImage >= valueThresholdLow) & (vImage <= valueThresholdHigh);
 
+%Putting it altogether into one image
+coloredObjectsMask = uint8(hueMask & saturationMask & valueMask);
+
+%Getting rid of excess pixels, smoothing borders, and filling regions
+smallestAcceptableArea = 100;
+coloredObjectsMask = uint8(bwareaopen(coloredObjectsMask, smallestAcceptableArea));
+structuringElement = strel('disk', 4);
+coloredObjectsMask = imclose(coloredObjectsMask, structuringElement);
+coloredObjectsMask = imfill(logical(coloredObjectsMask), 'holes');
+    
+measurements = regionprops(coloredObjectsMask);
+numberOfMeasurements = size(measurements, 1);
+if numberOfMeasurements > 1
+    fprint('Not a good image. Please try again.');
+else
+    boundingBox = measurements(1).BoundingBox;
+end
+
+croppedHand = imcrop(img, boundingBox);
+imshow(croppedHand);
 
 clear vid;
